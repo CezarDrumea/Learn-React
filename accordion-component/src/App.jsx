fa267ddc9373b318682c6ed458f4a914eabd8c35
Cezar <drumea.cezar@gmail.com>
2024-01-19 08:08:01 +0200

    add accordion component project and migrate steps project from create react app to vite

---
+++ accordion-component/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import './App.css';
+
+const faqs = [
+  {
+    title: 'Where are these chairs assembled?',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+  },
+  {
+    title: 'How long do I have to return my chair?',
+    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+  },
+  {
+    title: 'Do you ship to countries outside the EU?',
+    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+  },
+];
+
+const App = () => {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+};
+
+const Accordion = ({ data }) => {
+  return (
+    <div className='accordion'>
+      {data.map((item, i) => (
+        <Item {...item} num={i + 1} key={i} />
+      ))}
+    </div>
+  );
+};
+
+const Item = ({ num, title, text }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleIsOpen = () => setIsOpen((curState) => !curState);
+
+  return (
+    <div className={`item ${isOpen && 'open'}`} onClick={handleIsOpen}>
+      <p className='number'>{num < 9 ? `0${num}` : num}</p>
+      <p className='title'>{title}</p>
+      <p className='icon'>{isOpen ? '-' : '+'}</p>
+      {isOpen && <p className='content-box'>{text}</p>}
+    </div>
+  );
+};
+
+export default App;
