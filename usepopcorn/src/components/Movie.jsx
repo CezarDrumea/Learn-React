4d0a4d0e9604dfec4e0051722ecebbb2601fec2e
Cezar <drumea.cezar@gmail.com>
2024-01-21 23:13:00 +0200

    add usepopcorn project

---
+++ usepopcorn/src/components/Movie.jsx
@@ -0,0 +1,15 @@
+const Movie = ({ Poster, Title, Year }) => {
+  return (
+    <li>
+      <img src={Poster} alt={`${Title} poster`} />
+      <h3>{Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{Year}</span>
+        </p>
+      </div>
+    </li>
+  );
+};
+export default Movie;
---
+++ usepopcorn/src/components/WatchedMoviesList.jsx
@@ -0,0 +1,12 @@
+import WatchedMovie from './WatchedMovie';
+
+const WatchedMoviesList = ({ watched }) => {
+  return (
+    <ul className='list'>
+      {watched.map((movie) => (
+        <WatchedMovie key={movie.imdbID} {...movie} />
+      ))}
+    </ul>
+  );
+};
+export default WatchedMoviesList;
