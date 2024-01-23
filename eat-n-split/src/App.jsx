2b4cfa15d9f515ccc5240d08dfee85e17a63bca0
Cezar <drumea.cezar@gmail.com>
2024-01-23 20:32:49 +0200

    add how react works mini project

--- eat-n-split/src/App.jsx
+++ eat-n-split/src/App.jsx
@@ -4,24 +4,24 @@ import Sidebar from './components/Sidebar';
 import SplitBillForm from './components/SplitBillForm';
 
 const initialFriends = [
-  // {
-  //   id: 118836,
-  //   name: 'Clark',
-  //   image: 'https://i.pravatar.cc/48?u=118836',
-  //   balance: -7,
-  // },
-  // {
-  //   id: 933372,
-  //   name: 'Sarah',
-  //   image: 'https://i.pravatar.cc/48?u=933372',
-  //   balance: 20,
-  // },
-  // {
-  //   id: 499476,
-  //   name: 'Anthony',
-  //   image: 'https://i.pravatar.cc/48?u=499476',
-  //   balance: 0,
-  // },
+  {
+    id: 118836,
+    name: 'Clark',
+    image: 'https://i.pravatar.cc/48?u=118836',
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: 'Sarah',
+    image: 'https://i.pravatar.cc/48?u=933372',
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: 'Anthony',
+    image: 'https://i.pravatar.cc/48?u=499476',
+    balance: 0,
+  },
 ];
 
 const App = () => {
@@ -37,7 +37,11 @@ const App = () => {
         setClickedFriend={setClickedFriend}
       />
       {clickedFriend && (
-        <SplitBillForm {...clickedFriend} setFriends={setFriends} />
+        <SplitBillForm
+          {...clickedFriend}
+          setFriends={setFriends}
+          key={clickedFriend.id}
+        />
       )}
     </div>
   );
